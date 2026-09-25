861940d11212f388e5497fa123cdc3945606520e
Vinicius Silva <viniciuspdasilva.dev@gmail.com>
2026-09-24 21:44:22 -0300

    Add CORS to backend and improve frontend with new UI components
    Enabled CORS middleware in the FastAPI backend for better cross-origin support. Updated frontend with a redesigned interface, product management features, and integrated API handling. Enhanced README with detailed setup instructions and standardized repository structure.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 
 from src.infra.database import schema, Base
+from fastapi.middleware.cors import CORSMiddleware
 from src.presentation.routes import router
 
 Base.metadata.create_all(schema)
 
 app = FastAPI(title="Inventory API MVP")
+
+# 2. ADICIONA O CORS IMEDIATAMENTE AQUI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Permite o seu HTML local (porta 5500, file://, etc)
+    allow_credentials=True,
+    allow_methods=["*"], # Fundamental para permitir GET, POST, PUT, DELETE e OPTIONS
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
